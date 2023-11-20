3dce538030bb1ed0d3484d6a231b95161a97aaf0
Daniel Tabansi <dctabansi@gmail.com>
2023-11-20 04:05:31 -0500

    Added dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     // Compose Navigation
     implementation(libs.androidx.navigation.compose)
