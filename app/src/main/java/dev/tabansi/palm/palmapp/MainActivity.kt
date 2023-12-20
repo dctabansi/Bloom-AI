5e7f204d8ff4c7c802547085be71757be0b9dc72
Daniel Tabansi <dctabansi@gmail.com>
2023-12-20 16:30:00 -0500

    Final state of Palm project

--- app/src/main/java/dev/tabansi/palm/palmapp/MainActivity.kt
+++ app/src/main/java/dev/tabansi/palm/palmapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import dev.tabansi.palm.palmapp.sample.SampleChatUi
+import dev.tabansi.palm.palmapp.sample.palm.SampleChatUi
 import dev.tabansi.palm.palmapp.ui.theme.PalmTheme
 
 class MainActivity : ComponentActivity() {
